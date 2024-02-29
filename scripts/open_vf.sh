0e81540fb7b47e2a9c951d69d88905d5d8ad7e1a
X1aoyang <X1aoyang@node0.x1aoyang-194090.adslremotememory-pg0.utah.cloudlab.us>
2024-02-29 07:03:49 -0700

    update config scripts

--- scripts/open_vf.sh
+++ scripts/open_vf.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-ip link set enp129s0f0v0 up
-ifconfig enp129s0f0v0 10.10.1.10/24 up
-ip link set enp129s0f0v1 up
-ifconfig enp129s0f0v0 10.10.1.11/24 up
-ip link set enp129s0f0v2 up
-ifconfig enp129s0f0v0 10.10.1.12/24 up
-ip link set enp129s0f0v3 up
-ifconfig enp129s0f0v0 10.10.1.13/24 up
+echo 4 > /sys/class/net/enp65s0f0np0/device/sriov_numvfs
+
+./set_mac.sh 4 enp65s0f0np0
+
+ip link set enp65s0f0v0 up
+ifconfig enp65s0f0v0 10.10.1.10/24 up
+ip link set enp65s0f0v1 up
+ifconfig enp65s0f0v0 10.10.1.11/24 up
+ip link set enp65s0f0v2 up
+ifconfig enp65s0f0v0 10.10.1.12/24 up
+ip link set enp65s0f0v3 up
+ifconfig enp65s0f0v0 10.10.1.13/24 up
--- scripts/set_mac.sh
+++ scripts/set_mac.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-pcielist=(`ibdev2netdev -v | awk 'NR>=7' | awk '{print $1,ORS=" "}'`)
+pcielist=(`ibdev2netdev -v | awk 'NR>="'$1+1'"' | awk '{print $1,ORS=" "}'`)
 for i in ${pcielist[@]} 
 do    
 	echo $i > /sys/bus/pci/drivers/mlx5_core/unbind
 done
 for i in $(seq 0 $[${#pcielist[@]}-1]) 
 do
-     ip link set $1 vf $i mac 00:11:22:$(openssl rand -hex 3 | sed 's/../&:/g; s/.$//')
+     ip link set $2 vf $i mac 00:11:22:$(openssl rand -hex 3 | sed 's/../&:/g; s/.$//')
 done
 for i in ${pcielist[@]}
 do
