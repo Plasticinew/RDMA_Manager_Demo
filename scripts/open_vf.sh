3dc1cf94b02cdc9f9ff82ab57b46f48338f826c9
X1aoyang <X1aoyang@node1.x1aoyang-194090.adslremotememory-pg0.utah.cloudlab.us>
2024-03-03 20:13:23 -0700

    sync

--- scripts/open_vf.sh
+++ scripts/open_vf.sh
@@ -7,8 +7,8 @@ echo 4 > /sys/class/net/enp65s0f0np0/device/sriov_numvfs
 ip link set enp65s0f0v0 up
 ifconfig enp65s0f0v0 10.10.1.10/24 up
 ip link set enp65s0f0v1 up
-ifconfig enp65s0f0v0 10.10.1.11/24 up
+ifconfig enp65s0f0v1 10.10.1.11/24 up
 ip link set enp65s0f0v2 up
-ifconfig enp65s0f0v0 10.10.1.12/24 up
+ifconfig enp65s0f0v2 10.10.1.12/24 up
 ip link set enp65s0f0v3 up
-ifconfig enp65s0f0v0 10.10.1.13/24 up
+ifconfig enp65s0f0v3 10.10.1.13/24 up
