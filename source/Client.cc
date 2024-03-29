14468029128774ef4f7dd236ac6d0e4834f89447
X1aoyang <X1aoyang@node0.x1aoyang-198297.adslremotememory-pg0.utah.cloudlab.us>
2024-03-28 21:04:16 -0600

    add read latency check

--- source/Client.cc
+++ source/Client.cc
@@ -3,6 +3,17 @@
 
 const uint64_t page_size = 2*1024*1024;
 
+const uint64_t iter = 1000*1000;
+
+void do_read(rdmanager::vQP* vqp, void* addr, uint32_t rkey) {
+    printf("%lu\n", TIME_NOW);
+    for(uint64_t i = 0; i < iter; i++){
+        vqp->read(addr, page_size, (void*)0x1000000, rkey);
+        printf("%lu\n", TIME_NOW);
+    }
+    return;
+}
+
 int main() {
     std::vector<PigeonDevice> skip_device_list;
     // std::vector<std::string> named_device_list = {"mlx5_2", "mlx5_3"};
@@ -10,6 +21,7 @@ int main() {
     std::vector<PigeonDevice> named_device_list = {{"mlx5_4", "10.10.1.10"}, {"mlx5_5", "10.10.1.11"}};
     rdmanager::vContext vcontext(&skip_device_list, &named_device_list);
     void* addr = mmap(0, page_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, -1, 0);
+    memset(addr, 1, page_size);
     vcontext.memory_register(addr, page_size);
     rdmanager::vQP* vqp = vcontext.create_vQP_connecter("10.10.1.2", "1145");
     int* data = (int*)addr;
@@ -19,10 +31,10 @@ int main() {
         uint32_t rkey;
         scanf("%s %u", cmd, &rkey);
         if(strcmp(cmd, "read") == 0) {
-            vqp->read(addr, page_size, (void*)0x1000000, rkey);
-            for(int i = 0; i < 100; i ++) {
-                printf("%d ", data[i]);
-            }
+            std::thread* read_thread = new std::thread(&do_read, vqp, addr, rkey);
+            // for(int i = 0; i < 100; i ++) {
+            //     printf("%d ", data[i]);
+            // }
         } else if(strcmp(cmd, "write") == 0) {
             for(int i = 0; i < 100; i ++) {
                 data[i] += i;
