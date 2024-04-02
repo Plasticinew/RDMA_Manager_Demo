3b8f9e6d187004db5921deca186b197aa8f787f5
X1aoyang <X1aoyang@node0.x1aoyang-198297.adslremotememory-pg0.utah.cloudlab.us>
2024-04-01 21:11:54 -0600

    new trace

--- source/Client.cc
+++ source/Client.cc
@@ -14,16 +14,30 @@ void do_read(rdmanager::vQP* vqp, void* addr, uint32_t rkey) {
     return;
 }
 
+void do_switch(rdmanager::vQP* vqp, void* addr, uint32_t rkey) {
+    auto start_time = TIME_NOW;
+    for(uint64_t i = 0; i < iter/2; i++){
+        vqp->read(addr, page_size, (void*)0x1000000, rkey);
+        printf("%lu\n", TIME_DURATION_US(start_time, TIME_NOW));
+    }
+    vqp->switch_card();
+    for(uint64_t i = 0; i < iter/2; i++){
+        vqp->read(addr, page_size, (void*)0x1000000, rkey);
+        printf("%lu\n", TIME_DURATION_US(start_time, TIME_NOW));
+    }
+    return;
+}
+
 int main() {
     std::vector<PigeonDevice> skip_device_list;
     // std::vector<std::string> named_device_list = {"mlx5_2", "mlx5_3"};
     // std::vector<std::string> named_device_list = {"mlx5_4"};
-    std::vector<PigeonDevice> named_device_list = {{"mlx5_4", "10.10.1.10"}, {"mlx5_5", "10.10.1.11"}};
+    std::vector<PigeonDevice> named_device_list = {{"mlx5_1", "10.10.1.1"}, {"mlx5_3", "10.10.1.3"}};
     rdmanager::vContext vcontext(&skip_device_list, &named_device_list);
     void* addr = mmap(0, page_size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS|MAP_HUGETLB, -1, 0);
     memset(addr, 1, page_size);
     vcontext.memory_register(addr, page_size);
-    rdmanager::vQP* vqp = vcontext.create_vQP_connecter("10.10.1.2", "1145");
+    rdmanager::vQP* vqp = vcontext.create_vQP_connecter("10.10.1.4", "1145");
     int* data = (int*)addr;
     // loop parse input command, each line like: read/write [rkey]
     while(1) {
@@ -44,7 +58,7 @@ int main() {
                 data[i] = 0;
             }
         } else if(strcmp(cmd, "switch") == 0) {
-            vqp->switch_card();
+            std::thread* read_thread = new std::thread(&do_switch, vqp, addr, rkey);
         }
     }
 
